edf7a416e006a3953aadf6254d9e72abbb8d1f43
Pila <pat@medicuja.com>
2020-11-12 03:50:05 +0100

    burn

--- config/dev.js
+++ config/dev.js
@@ -1,10 +1,10 @@
 module.exports = {
   googleClientID:
-      '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://pila:KEjHTKtNH0GzwhVJ@cluster0-shard-00-00.nwhpw.mongodb.net:27017,cluster0-shard-00-01.nwhpw.mongodb.net:27017,cluster0-shard-00-02.nwhpw.mongodb.net:27017/node_blog?ssl=true&replicaSet=atlas-1wouqh-shard-0&authSource=admin&retryWrites=true&w=majority',
+      '<clientId>',
+  googleClientSecret: 'secret',
+  mongoURI: '',
   cookieKey: '123123123',
   redisUrl: 'redis://127.0.0.1:6379',
-  accessKeyId: 'AKIAZKONM3XSMBCDZLOD',
-  secretAccessKey: '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'
+  accessKeyId: '',
+  secretAccessKey: ''
 };
